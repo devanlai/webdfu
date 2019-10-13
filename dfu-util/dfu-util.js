d925766f58163f28db18e018f56a8531df2589c8
İhsan Kehribar <ihsan.kehribar@edelkrone.com>
2019-10-13 15:11:04 +0300

    Fixes "InvalidStateError: The device must have a configuration selected." problem in Chrome77 OSX

--- dfu-util/dfu-util.js
+++ dfu-util/dfu-util.js
@@ -75,6 +75,7 @@ var device = null;
             // Manually retrieve the interface name string descriptors
             let tempDevice = new dfu.Device(device_, interfaces[0]);
             await tempDevice.device_.open();
+            await tempDevice.device_.selectConfiguration(1);
             let mapping = await tempDevice.readInterfaceNames();
             await tempDevice.close();
 
@@ -368,7 +369,7 @@ var device = null;
                     }
                 }
             }
-            
+
             // Bind logging methods
             device.logDebug = logDebug;
             device.logInfo = logInfo;
@@ -622,7 +623,7 @@ var device = null;
                 configForm.reportValidity();
                 return false;
             }
-            
+
             if (device && firmwareFile != null) {
                 setLogContext(downloadLog);
                 clearLog(downloadLog);
